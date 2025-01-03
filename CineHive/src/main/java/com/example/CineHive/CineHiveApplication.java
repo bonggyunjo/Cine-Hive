a9518bfcc63522c109785e15fd4746963d2cebfd
jbg <kjbg4565388@gmail.com>
2025-01-03 21:33:38 +0900

    change project file

---
+++ CineHive/src/main/java/com/example/CineHive/CineHiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.CineHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineHiveApplication.class, args);
+	}
+
+}
