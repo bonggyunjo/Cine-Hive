2f71617a73f296ec0da7e3437bcc50e0ae44ea9c
jbg <kjbg4565388@gmail.com>
2025-01-05 19:18:43 +0900

    [Add]: KakaoUserInfo Dto 추가

---
+++ CineHive/src/main/java/com/example/CineHive/dto/KakaoUserInfo.java
@@ -0,0 +1,18 @@
+package com.example.CineHive.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoUserInfo {
+
+    private String id;
+    private String email;
+    private String nickname;
+}
