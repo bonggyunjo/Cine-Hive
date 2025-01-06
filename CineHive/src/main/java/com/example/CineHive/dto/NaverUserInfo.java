2378a1a1f65da8bc16f163cc13a66b061300965d
jbg <kjbg4565388@gmail.com>
2025-01-06 18:02:26 +0900

    [Add]: NaverUserInfo 추가(Dto)

---
+++ CineHive/src/main/java/com/example/CineHive/dto/NaverUserInfo.java
@@ -0,0 +1,25 @@
+package com.example.CineHive.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NaverUserInfo {
+    private String id;
+
+    private String naverId;
+
+    private String email;
+
+    private String nickname;
+
+    private String gender;
+
+    private String phone;
+}
