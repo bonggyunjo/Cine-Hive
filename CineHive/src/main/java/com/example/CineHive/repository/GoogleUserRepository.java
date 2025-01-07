1de5effa38fc9c473b04f7632a93f6c64f32c07b
jbg <kjbg4565388@gmail.com>
2025-01-07 14:42:43 +0900

    [Add]: Google ID로 사용자 조회 메서드 추가

--- CineHive/src/main/java/com/example/CineHive/repository/GoogleUserRepository.java
+++ CineHive/src/main/java/com/example/CineHive/repository/GoogleUserRepository.java
@@ -1,11 +1,12 @@
 package com.example.CineHive.repository;
 
 import com.example.CineHive.entity.GoogleUser;
-import com.example.CineHive.entity.KakaoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GoogleUserRepository extends JpaRepository<GoogleUser, Long> {
-    Optional<GoogleUser> findByKakaoId(String googleId);
+    Optional<GoogleUser> findByGoogleId(String googleId);
 }
