23b77469985471cd74f9b8d402259aaf8b8c052c
jbg <kjbg4565388@gmail.com>
2025-01-14 21:25:45 +0900

    [Add]: 회원가입 router 추가

---
+++ lay-out/src/View/SignUp.vue
@@ -0,0 +1,13 @@
+<template>
+<div id="signup">
+  test
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- lay-out/src/router/router.js
+++ lay-out/src/router/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/View/Home.vue';
+import SignUp from "@/View/SignUp.vue";
 
 Vue.use(Router);
 
 const routes = [
     { path: '/', component: Home },
+    {path: '/signup', component: SignUp}
 ];
 
 const router = new Router({
