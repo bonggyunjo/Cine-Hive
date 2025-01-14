f0057b74eb08dc055928df6173aa5ed1a9c8ae42
ssb3204 <ryanp3@naver.com>
2025-01-14 17:19:47 +0900

    주석 추가

--- CineHive/src/main/java/com/example/CineHive/CineHiveApplication.java
+++ CineHive/src/main/java/com/example/CineHive/CineHiveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CineHiveApplication {
-
+	//조봉균
 	public static void main(String[] args) {
 		SpringApplication.run(CineHiveApplication.class, args);
 	}
