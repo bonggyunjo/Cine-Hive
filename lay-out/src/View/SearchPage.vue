f846c8c5311d6330629eee96d6f01fa852566c62
jbg <kjbg4565388@gmail.com>
2025-02-04 18:30:54 +0900

    [Fix]: 검색 화면 UI 수정

--- lay-out/src/View/SearchPage.vue
+++ lay-out/src/View/SearchPage.vue
@@ -1,102 +1,83 @@
 <template>
   <div id="searchpage">
     <div v-if="movies.length || dramas.length || animations.length">
-      <h2 class="movie-title">영화</h2>
-      <div class="movie-slider" v-if="movies.length">
-
-        <div
-            class="movie-poster"
-            v-for="movie in movies"
-            :key="movie.id"
-            @click="openMovieDetails(movie)"
-        >
-          <img :src="getImageUrl(movie.posterPath)" alt="movie poster" />
+      <div class="category">
+        <h2 class="title">영화</h2>
+        <div class="grid-container">
+          <div
+              class="poster"
+              v-for="movie in movies"
+              :key="movie.id"
+              @click="openMovieDetails(movie)"
+          >
+            <img :src="getImageUrl(movie.posterPath)" alt="movie poster" />
+          </div>
         </div>
+        <p v-if="!movies.length" class="no-results">정보가 없습니다.</p>
       </div>
 
-      <h2 class="drama-title">드라마</h2>
-      <div class="drama-slider" v-if="dramas.length">
+      <div class="category">
+        <h2 class="title">드라마</h2>
+        <div class="grid-container">
+          <div
+              class="poster"
+              v-for="drama in dramas"
+              :key="drama.id"
+              @click="openDramaDetails(drama)"
+          >
+            <img :src="getImageUrl(drama.posterPath)" alt="drama poster" />
+          </div>
+        </div>
+        <p v-if="!dramas.length" class="no-results">정보가 없습니다.</p>
+      </div>
 
-        <div
-            class="drama-poster"
-            v-for="drama in dramas"
-            :key="drama.id"
-            @click="openDramaDetails(drama)"
-        >
-          <img :src="getImageUrl(drama.posterPath)" alt="drama poster" />
+      <div class="category">
+        <h2 class="title">애니메이션</h2>
+        <div class="grid-container">
+          <div
+              class="poster"
+              v-for="animation in animations"
+              :key="animation.id"
+              @click="openAnimationDetails(animation)"
+          >
+            <img :src="getImageUrl(animation.posterPath)" alt="animation poster" />
+          </div>
+        </div>
+        <p v-if="!animations.length" class="no-results">정보가 없습니다.</p>
       </div>
     </div>
-
-      <h2 class="animation-title">애니메이션</h2>
-      <div class="animation-slider" v-if="animations.length">
-
-        <div
-            class="animation-poster"
-            v-for="animation in animations"
-            :key="animation.id"
-            @click="openAnimationDetails(animation)"
-        >
-          <img :src="getImageUrl(animation.posterPath)" alt="animation poster" />
-        </div>
-        </div>
-    <div v-else>
-      <p>검색 결과가 없습니다.</p>
-    </div>
+    <p v-else class="no-results">검색 결과가 없습니다.</p>
 
     <!-- 영화 상세 정보 모달 -->
-    <div v-if="selectedMovie" class="movie-modal" @click.self="closeMovieDetails">
-      <div
-          class="movie-modal-backdrop"
-          :style="{
-          backgroundImage: 'url(https://image.tmdb.org/t/p/original' + selectedMovie.backdropPath + ')',
-        }"
-      >
-        <div class="movie-modal-content">
-          <h2>{{ selectedMovie.title }}</h2>
-          <p>{{ selectedMovie.overview || '설명 없음' }}</p>
-          <p>평점: {{ selectedMovie.voteAverage }}</p>
-          <p>출시일: {{ selectedMovie.releaseDate }}</p>
-          <button @click="closeMovieDetails">닫기</button>
-        </div>
+    <div v-if="selectedMovie" class="modal" @click.self="closeMovieDetails">
+      <div class="modal-content">
+        <h2>{{ selectedMovie.title }}</h2>
+        <p>{{ selectedMovie.overview || '설명 없음' }}</p>
+        <p>평점: {{ selectedMovie.voteAverage }}</p>
+        <p>출시일: {{ selectedMovie.releaseDate }}</p>
+        <button @click="closeMovieDetails">닫기</button>
       </div>
     </div>
 
-
-    <div v-if="selectedDrama" class="drama-modal" @click.self="closeDramaDetails">
-      <div
-          class="drama-modal-backdrop"
-          :style="{
-          backgroundImage: 'url(https://image.tmdb.org/t/p/original' + selectedDrama.backdropPath + ')',
-        }"
-      >
-        <div class="drama-modal-content">
-          <h2>{{ selectedDrama.name }}</h2>
-          <p>{{ selectedDrama.overview || '설명 없음' }}</p>
-          <p>평점: {{ selectedDrama.voteAverage }}</p>
-          <p>출시일: {{ selectedDrama.releaseDate }}</p>
-          <button @click="closeDramaDetails">닫기</button>
-          </div>
+    <div v-if="selectedDrama" class="modal" @click.self="closeDramaDetails">
+      <div class="modal-content">
+        <h2>{{ selectedDrama.name }}</h2>
+        <p>{{ selectedDrama.overview || '설명 없음' }}</p>
+        <p>평점: {{ selectedDrama.voteAverage }}</p>
+        <p>출시일: {{ selectedDrama.releaseDate }}</p>
+        <button @click="closeDramaDetails">닫기</button>
       </div>
-      </div>
+    </div>
 
-
-      <div v-if="selectedAnimation" class="animation-modal" @click.self="closeAnimationDetails">
-        <div
-            class="animation-modal-backdrop"
-            :style="{
-            backgroundImage: 'url(https://image.tmdb.org/t/p/original' + selectedAnimation.backdropPath + ')',
-            }"
-        >
-          <div class="animation-modal-content">
-            <h2>{{ selectedAnimation.name }}</h2>
-            <p>{{ selectedAnimation.overview || '설명 없음' }}</p>
-            <p>평점: {{ selectedAnimation.voteAverage }}</p>
-            <p>출시일: {{ selectedAnimation.releaseDate }}</p>
-            <button @click="closeDramaDetails">닫기</button>
-          </div>
-        </div>
+    <div v-if="selectedAnimation" class="modal" @click.self="closeAnimationDetails">
+      <div class="modal-content">
+        <h2>{{ selectedAnimation.name }}</h2>
+        <p>{{ selectedAnimation.overview || '설명 없음' }}</p>
+        <p>평점: {{ selectedAnimation.voteAverage }}</p>
+        <p>출시일: {{ selectedAnimation.releaseDate }}</p>
+        <button @click="closeAnimationDetails">닫기</button>
       </div>
-  </div>
+    </div>
   </div>
 </template>
 
@@ -104,60 +85,46 @@
 export default {
   data() {
     return {
-      searchQuery: this.$route.query.q,  // 검색어
-      movies: [],  // 검색된 영화 결과
-      dramas: [], // 검색된 드라마 결과
-      animations: [], // 검색된 애니메이션 결과
-      selectedMovie: null,  // 선택된 영화
-      selectedDrama: null,  // 선택된 드라마
-      selectedAnimation: null, // 선택된 애니메이션
+      searchQuery: this.$route.query.q,
+      movies: [],
+      dramas: [],
+      animations: [],
+      selectedMovie: null,
+      selectedDrama: null,
+      selectedAnimation: null,
     };
   },
   mounted() {
     this.fetchSearchResults();
   },
   watch: {
-    // 검색어가 변경되면 결과를 갱신
-    '$route.query.q': function(newQuery) {
+    '$route.query.q': function (newQuery) {
       this.searchQuery = newQuery;
       this.fetchSearchResults();
     }
   },
   methods: {
-    // 영화 검색 결과 가져오기
     fetchSearchResults() {
-
       const moviesData = this.$route.query.movies;
       const dramasData = this.$route.query.dramas;
       const animationsData = this.$route.query.animations;
 
-      if (moviesData) {
-        this.movies = JSON.parse(moviesData);  // JSON 문자열을 객체로 변환
-      }
-      if(dramasData){
-        this.dramas = JSON.parse(dramasData);
-      }
-      if(animationsData){
-        this.animations = JSON.parse(animationsData);
-      }
+      this.movies = moviesData ? JSON.parse(moviesData) : [];
+      this.dramas = dramasData ? JSON.parse(dramasData) : [];
+      this.animations = animationsData ? JSON.parse(animationsData) : [];
     },
-    // 영화의 포스터 이미지를 올바른 URL로 변환
     getImageUrl(path) {
       return path ? `https://image.tmdb.org/t/p/w500${path}` : '/default-poster.jpg';
     },
-    // 영화 포스터 클릭 시 상세 정보 모달 열기
     openMovieDetails(movie) {
-      this.$router.push({ name: 'MovieDetail', params: { id: movie.id } });
+      this.selectedMovie = movie;
     },
-    // 드라마 포스터 클릭 시 상세 정보 모달 열기
     openDramaDetails(drama) {
-      this.$router.push({ name: 'DramaDetail', params: { id: drama.id } });
+      this.selectedDrama = drama;
     },
-    // 애니메이션 포스터 클릭 시 상세 정보 모달 열기
     openAnimationDetails(animation) {
-      this.$router.push({ name: 'AnimationDetail', params: { id: animation.id } });
+      this.selectedAnimation = animation;
     },
-    // 검색 버튼 클릭 시 새로운 검색어로 URL 갱신
     closeMovieDetails() {
       this.selectedMovie = null;
     },
@@ -172,112 +139,91 @@ export default {
 </script>
 
 <style scoped>
-
-.animation-title,
-.movie-title,
-.drama-title{
-  position: relative;
-  bottom: 10px;
-  left: 0.5%;
-  text-align: left;
-}
-#searchpage{
-  height: 900px;
-  flex: 1;
-  background-color : black;
+#searchpage {
+  min-height: 100vh;
+  background-color: black;
   color: white;
-}
-.animation-slider,
-.drama-slider,
-.movie-slider {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 10px;
-  margin-bottom: 20px;
-}
-.animation-poster img:hover,
-.drama-poster img:hover,
-.movie-poster img:hover {
-  transform: scale(1.1);
-}
-
-.animation-poster img,
-.drama-poster img,
-.movie-poster img {
-  width: 200px;
-  height: 300px;
-  cursor: pointer;
-  transition: transform 0.3s;
-}
-
-/* 정보 모달 */
-.animation-modal,
-.drama-modal,
-.movie-modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.8);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-}
-
-.animation-modal-backdrop,
-.drama-modal-backdrop,
-.movie-modal-backdrop {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-}
-
-.animation-modal-content,
-.drama-modal-content,
-.movie-modal-content {
-  background-color: rgba(0, 0, 0, 0.8);
   padding: 20px;
-  border-radius: 8px;
-  max-width: 600px;
-  text-align: center;
-  color: white;
 }
 
-.animation-modal-content button,
-.drama-modal-content button,
-.movie-modal-content button {
-  margin-top: 20px;
-  padding: 10px;
-  background-color: #1a1a1a;
+/* 카테고리 스타일 */
+.category {
+  margin-bottom: 30px;
+  text-align: left; /* 추가: 카테고리를 왼쪽 정렬 */
+}
+
+
+/* 제목 스타일 */
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  text-align: left; /* 추가: 텍스트를 왼쪽 정렬 */
+}
+
+
+/* 포스터 컨테이너 */
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 15px;
+  justify-content: center;
+}
+
+/* 포스터 스타일 */
+.poster img {
+  width: 100%;
+  border-radius: 10px;
+  transition: transform 0.3s, box-shadow 0.3s;
+}
+
+.poster img:hover {
+  transform: scale(1.05);
+  box-shadow: 0 4px 10px rgba(255, 255, 255, 0.2);
+  cursor: pointer;
+}
+
+/* 검색 결과 없을 때 */
+.no-results {
+  text-align: center;
+  color: gray;
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+/* 모달 스타일 */
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: black;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  width: 400px;
+}
+
+.modal-content button {
+  margin-top: 15px;
+  padding: 10px 20px;
+  background: red;
   border: none;
   color: white;
   cursor: pointer;
   border-radius: 5px;
 }
 
-.search-bar input {
-  width: 80%;
-  max-width: 600px;
-  padding: 10px;
-  font-size: 16px;
-  border: none;
-  border-radius: 5px;
-}
-
-.search-bar input:focus {
-  outline: none;
-  box-shadow: 0 0 5px rgba(255, 0, 0, 0.8); /* 포커스 시 효과 */
+.modal-content button:hover {
+  background: darkred;
 }
 </style>
