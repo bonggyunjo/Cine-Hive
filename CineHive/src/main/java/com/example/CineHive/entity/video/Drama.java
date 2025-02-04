9b0baba82f24b48cd89e89948b7511a75eef42fd
jbg <kjbg4565388@gmail.com>
2025-02-04 14:54:51 +0900

    [Add]: 드라마 감독 정보인 Director 엔티티 생성

--- CineHive/src/main/java/com/example/CineHive/entity/video/Drama.java
+++ CineHive/src/main/java/com/example/CineHive/entity/video/Drama.java
@@ -1,5 +1,6 @@
 package com.example.CineHive.entity.video;
 
+import com.example.CineHive.entity.credit.drama.Director;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,7 +31,8 @@ public class Drama {
     private boolean adult;
     private String originalLanguage;
 
-    @ElementCollection
-    private List<Integer> genreIds;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "drama_id") // 외래 키 설정
+    private List<Director> directors; // 감독 리스트    private List<Integer> genreIds;
 
 }
